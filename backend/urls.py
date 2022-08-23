81f4094dd6ab2a337af1be08efd66118fae7e369
kgredziszewski <gredziszewski.k@gmail.com>
2022-08-23 22:21:54 +0200

    url

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    re_path('.*', TemplateView.as_view(template_name='index.html')),
     path('', include('restapi.urls')),
 ]

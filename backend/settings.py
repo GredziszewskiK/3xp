eb2252042d983aaee0051e9fde51f5edbf0e278d
kgredziszewski <gredziszewski.k@gmail.com>
2022-08-23 21:23:11 +0200

    try3

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ from pathlib import Path
 from pickle import TRUE 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().root.root
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):

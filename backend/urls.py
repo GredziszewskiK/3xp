3c1e81e67bb71c76fa702187d90a31861c2f2f4c
kgredziszewski <gredziszewski.k@gmail.com>
2022-08-23 22:25:16 +0200

    url2

--- backend/urls.py
+++ backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     re_path('.*', TemplateView.as_view(template_name='index.html')),
